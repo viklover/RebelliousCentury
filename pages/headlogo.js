1c6216bdbf62a1f3da09ca7923140df4c4e1d344
Mihail Vorobyev <60448208+viklover@users.noreply.github.com>
2021-12-18 00:37:34 +0500

    Addition link to ithub college and change style of footer menu

--- pages/headlogo.js
+++ pages/headlogo.js
@@ -1,5 +1,13 @@
+// HEADER WEBSITE LOGO
 let container = document.getElementById('head').getElementsByClassName('container')[0];
 
 container.onclick = function (e) {
     document.location.href = '../../index.html';
 }
+
+// FOOTER IHUB LOGO
+let logo = document.getElementById('foot').getElementsByClassName('logo')[0];
+
+logo.addEventListener('click', function () {
+    document.location.href = 'https://ekat.ithub.ru';
+});
